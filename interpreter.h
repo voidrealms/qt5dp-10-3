3656ac515dbf0b093a61769c3348ea04ef0e8b7d
voidrealms <bcairns@voidrealms.com>
2020-08-25 13:35:57 -0400

    first commit

---
+++ interpreter.h
@@ -0,0 +1,19 @@
+#ifndef INTERPRETER_H
+#define INTERPRETER_H
+
+#include <QObject>
+#include <QDebug>
+
+class Interpreter : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Interpreter(QObject *parent = nullptr);
+
+    int interpret(QString data);
+
+signals:
+
+};
+
+#endif // INTERPRETER_H
